faebb163bc5a50f97d2b0b85a1c991168cd3ef77
Hyper-TH <twila.shayne.habab@outlook.com>
2023-11-07 21:34:20 +0000

    Modified Votes Staging

--- VotesFactStaging.sql
+++ VotesFactStaging.sql
@@ -24,6 +24,7 @@ SELECT x.viewerID, e.ed_sk, e.edYear, p.part_sk, v.catID, o.voteMode, o.vote, o.
     JOIN stage_viewer_category v ON (o.VOTE_CATEGORY = v.catID);
 
 
+
 DROP SEQUENCE IF EXISTS votes_seq;
 CREATE SEQUENCE votes_seq
 START WITH 1
@@ -34,7 +35,8 @@ UPDATE stage_votes set voteID = (NEXT value FOR votes_seq);
 
 UPDATE stage_votes SET catName = 'Jury' WHERE catID = 1;
 UPDATE stage_votes SET catName = 'Audience' WHERE catID = 2;
-   
+  
+ALTER TABLE stage_votes DROP COLUMN cost;
 ALTER TABLE stage_votes ADD COLUMN cost DECIMAL (18,5);
 
 UPDATE stage_votes e SET cost = (CAST(e.vote AS DECIMAL(18,5))) * .2
@@ -60,9 +62,9 @@ WHERE
 	e.edYear >= 2016 AND e.edYear <= 2022 AND 
 	e.voteMode IN ('TV', 'Phone') AND 
 	e.catName IN ('Audience');
+	
 
 UPDATE stage_votes e SET cost = (CAST(e.vote AS DECIMAL(18,5))) * 0
 WHERE e.catName IN ('Jury');
-
 
 SELECT * FROM stage_votes;
